57b205ccb49dc6b112ae4b57281f0e0bd6cdb8ab
MudaDayo <dan@chicu.ro>
2023-03-11 00:19:44 +0100

    jeu qui marche

--- Assets/FPSController.cs
+++ Assets/FPSController.cs
@@ -88,7 +88,7 @@ public class FPSController : MonoBehaviour
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            //playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
 
