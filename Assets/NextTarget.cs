5a89596f9abfcf030e87b3164ac76556062ed0ac
MudaDayo <dan@chicu.ro>
2023-03-11 00:13:56 +0100

    added triggers for waypoints

--- Assets/NextTarget.cs
+++ Assets/NextTarget.cs
@@ -8,15 +8,25 @@ public class NextTarget : MonoBehaviour
 
     public GameObject[] checkPoints;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log(other);
+        Debug.Log(checkPoints[0]);
+
+        if(other.name == checkPoints[0].name){
+            soundCues[0].GetComponent<AudioSource>().enabled = false;
+            soundCues[2].GetComponent<AudioSource>().enabled = true;
+        }else if(other.name == checkPoints[1].name){
+            soundCues[1].GetComponent<AudioSource>().enabled = true;
+        }else if(other.name == checkPoints[2].name){
+            soundCues[1].GetComponent<AudioSource>().enabled = false;
+        }else if(other.name == checkPoints[3].name){
+            soundCues[2].GetComponent<AudioSource>().enabled = false;
+            soundCues[3].GetComponent<AudioSource>().enabled = true;
+        }else if(other.name == checkPoints[4].name){
+            soundCues[3].GetComponent<AudioSource>().enabled = false;
+            soundCues[4].GetComponent<AudioSource>().enabled = true;
+        }
     }
 }
