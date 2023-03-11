a3632b1d28e0fc335e856b8162935a2f9e554270
MudaDayo <dan@chicu.ro>
2023-03-11 14:05:27 +0100

    sand walk works

--- Assets/NextTarget.cs
+++ Assets/NextTarget.cs
@@ -8,25 +8,32 @@ public class NextTarget : MonoBehaviour
 
     public GameObject[] checkPoints;
 
+    public AudioClip sandWalk;
+
+    public AudioClip normalWalk;
+
+    public AudioClip waterWalk;
+
+    AudioSource walkSound;
+
+    void Start(){
+        walkSound = GetComponent<AudioSource>();
+        walkSound.clip = normalWalk;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other);
-        Debug.Log(checkPoints[0]);
-
-        if(other.name == checkPoints[0].name){
-            soundCues[0].GetComponent<AudioSource>().enabled = false;
-            soundCues[2].GetComponent<AudioSource>().enabled = true;
-        }else if(other.name == checkPoints[1].name){
-            soundCues[1].GetComponent<AudioSource>().enabled = true;
-        }else if(other.name == checkPoints[2].name){
-            soundCues[1].GetComponent<AudioSource>().enabled = false;
-        }else if(other.name == checkPoints[3].name){
-            soundCues[2].GetComponent<AudioSource>().enabled = false;
-            soundCues[3].GetComponent<AudioSource>().enabled = true;
-        }else if(other.name == checkPoints[4].name){
-            soundCues[3].GetComponent<AudioSource>().enabled = false;
-            soundCues[4].GetComponent<AudioSource>().enabled = true;
+        
+        if(other.tag == "sand"){
+            walkSound.clip = sandWalk;
+        }else if(other.tag == "water"){
+            walkSound.clip = waterWalk;
+        }else if(other.tag == "normal"){
+            walkSound.clip = normalWalk;
         }
+
     }
+
+
 }
