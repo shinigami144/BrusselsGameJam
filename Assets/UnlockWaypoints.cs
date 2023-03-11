9b52355983dd8edc71820576fc124b6f72219d83
MudaDayo <dan@chicu.ro>
2023-03-11 20:30:05 +0100

    unlock waypoints

---
+++ Assets/UnlockWaypoints.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockWaypoints : MonoBehaviour
+{
+    public GameObject [] waypoints;
+
+    private void OnTriggerEnter(Collider other){
+        
+    }
+}
