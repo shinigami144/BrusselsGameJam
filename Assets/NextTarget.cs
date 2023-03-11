4fed5902e5b04fcb64fffbfba57c5ee464ed254f
MudaDayo <dan@chicu.ro>
2023-03-11 09:37:35 +0100

    some changes

--- Assets/NextTarget.cs
+++ Assets/NextTarget.cs
@@ -15,18 +15,18 @@ public class NextTarget : MonoBehaviour
         Debug.Log(checkPoints[0]);
 
         if(other.name == checkPoints[0].name){
-            soundCues[0].GetComponent<AudioSource>().enabled = false;
-            soundCues[2].GetComponent<AudioSource>().enabled = true;
+            soundCues[0].GetComponent<AudioSource>().Stop();
+            soundCues[1].GetComponent<AudioSource>().Play();
         }else if(other.name == checkPoints[1].name){
-            soundCues[1].GetComponent<AudioSource>().enabled = true;
+            soundCues[2].GetComponent<AudioSource>().Play();
         }else if(other.name == checkPoints[2].name){
-            soundCues[1].GetComponent<AudioSource>().enabled = false;
+            soundCues[1].GetComponent<AudioSource>().Stop();
         }else if(other.name == checkPoints[3].name){
-            soundCues[2].GetComponent<AudioSource>().enabled = false;
-            soundCues[3].GetComponent<AudioSource>().enabled = true;
+            soundCues[2].GetComponent<AudioSource>().Stop();
+            soundCues[3].GetComponent<AudioSource>().Play();
         }else if(other.name == checkPoints[4].name){
-            soundCues[3].GetComponent<AudioSource>().enabled = false;
-            soundCues[4].GetComponent<AudioSource>().enabled = true;
+            soundCues[3].GetComponent<AudioSource>().Stop();
+            soundCues[4].GetComponent<AudioSource>().Play();
         }
     }
 }
