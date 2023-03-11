abcf77b5eb615d09556c5237e0eb4e2049ddecb2
MudaDayo <dan@chicu.ro>
2023-03-11 20:37:51 +0100

    unloakcaewaypoeitnst

--- Assets/UnlockWaypoints.cs
+++ Assets/UnlockWaypoints.cs
@@ -7,6 +7,12 @@ public class UnlockWaypoints : MonoBehaviour
     public GameObject [] waypoints;
 
     private void OnTriggerEnter(Collider other){
-        
+        switch (other.name){
+            case "Checkpoint":
+                waypoints[0].GetComponent<WayPointScript>().lockWayPoint = false;
+                
+                
+            break;
+        }
     }
 }
